c8663e9a754461a71bbd166632e3ca67088dc180
WasimX <xw451mx@gmail.com>
2019-02-12 21:57:30 +0000

    Contact us page

--- app/src/main/java/com/braduni/wasim/asthmaapp/Contact_us_Fragment.java
+++ app/src/main/java/com/braduni/wasim/asthmaapp/Contact_us_Fragment.java
@@ -1,5 +1,7 @@
 package com.braduni.wasim.asthmaapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -15,4 +17,14 @@ public class Contact_us_Fragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.activity_contact_us, container, false);
     }
+
+    protected void openDialer(){
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+
+        intent.setData(Uri.parse("tel:" + "03002225800"));
+        // Start the dialer app activity with number
+        startActivity(intent);
+    }
+
 }
