a416c63a0588a55f99216c738882bc5a58283b90
WasimX <xw451mx@gmail.com>
2019-02-17 21:55:52 +0000

    added activities

---
+++ app/src/main/java/com/braduni/wasim/asthmaapp/ActionPlanActivity.java
@@ -0,0 +1,13 @@
+package com.braduni.wasim.asthmaapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ActionPlanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_action_plan);
+    }
+}
--- app/src/main/java/com/braduni/wasim/asthmaapp/Em_Support_Fragment.java
+++ app/src/main/java/com/braduni/wasim/asthmaapp/Em_Support_Fragment.java
@@ -1,5 +1,6 @@
 package com.braduni.wasim.asthmaapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,12 +8,42 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
-public class Em_Support_Fragment extends Fragment {
+public class Em_Support_Fragment extends Fragment implements View.OnClickListener {
+
+    private ImageButton image_button_AFA;
+    private ImageButton image_button_VAP;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_em_support, container, false);
+
+        View rootview = inflater.inflate(R.layout.activity_em_support, container, false);
+
+        image_button_AFA = rootview.findViewById(R.id.image_button_AFA);
+        image_button_VAP = rootview.findViewById(R.id.image_button_VAP);
+
+        image_button_AFA.setOnClickListener(this);
+        image_button_VAP.setOnClickListener(this);
+
+        return rootview;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        if (v == image_button_AFA) {
+            //Toast.makeText(getContext(), "test", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(getActivity(), FirstAidActivity.class));
+        }
+
+        if (v == image_button_VAP) {
+            // Toast.makeText(getContext(), "test", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(getActivity(), ActionPlanActivity.class));
+        }
     }
 }
+
