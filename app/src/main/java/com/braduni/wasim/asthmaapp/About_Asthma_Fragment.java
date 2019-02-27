b2812dae5875458b8db12fd11d2d78a5e0ea936d
WasimX <xw451mx@gmail.com>
2019-02-27 23:27:24 +0000

    learnt to do fragments

--- app/src/main/java/com/braduni/wasim/asthmaapp/About_Asthma_Fragment.java
+++ app/src/main/java/com/braduni/wasim/asthmaapp/About_Asthma_Fragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,19 +22,26 @@ public class About_Asthma_Fragment extends Fragment implements View.OnClickListe
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View rootview = inflater.inflate(R.layout.activity_about_asthma, container, false);
+        View v = inflater.inflate(R.layout.activity_about_asthma, container, false);
 
-        image_button_trigger = rootview.findViewById(R.id.image_button_trigger);
-        image_button_medication = rootview.findViewById(R.id.image_button_medication);
-        image_button_moreinfo = rootview.findViewById(R.id.image_button_moreinfo);
-        image_button_symptoms = rootview.findViewById(R.id.image_button_symptoms);
+        image_button_trigger = v.findViewById(R.id.image_button_trigger);
+        image_button_medication = v.findViewById(R.id.image_button_medication);
+        image_button_moreinfo = v.findViewById(R.id.image_button_moreinfo);
+        image_button_symptoms = v.findViewById(R.id.image_button_symptoms);
 
-        image_button_trigger .setOnClickListener(this);
+        image_button_trigger .setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                test trigger  = new test();
+                FragmentManager manager = getFragmentManager();
+                manager.beginTransaction().replace(R.id.fragment_container, new TriggerActivity()).commit();
+            }
+        });
         image_button_medication.setOnClickListener(this);
         image_button_moreinfo .setOnClickListener(this);
         image_button_symptoms .setOnClickListener(this);
 
-        return rootview;
+        return v;
     }
 
     @Override
