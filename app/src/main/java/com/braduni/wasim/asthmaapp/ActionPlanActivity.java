43d23199fcf528802d4ae4094eff6cd8055e9360
WasimX <xw451mx@gmail.com>
2019-03-26 17:10:09 +0000

    green zone layouts

--- app/src/main/java/com/braduni/wasim/asthmaapp/ActionPlanActivity.java
+++ app/src/main/java/com/braduni/wasim/asthmaapp/ActionPlanActivity.java
@@ -10,12 +10,9 @@ import android.view.LayoutInflater;
         import android.view.View;
         import android.view.ViewGroup;
         import android.widget.Button;
+import android.widget.TextView;
 
-public class ActionPlanActivity extends Fragment implements View.OnClickListener {
-
-    private Button code_green;
-    private Button code_yellow;
-    private Button code_red;
+public class ActionPlanActivity extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -23,33 +20,20 @@ public class ActionPlanActivity extends Fragment implements View.OnClickListener
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.activity_action_plan, container, false);
 
-        code_green = v.findViewById(R.id.code_green_btn);
-        code_yellow = v.findViewById(R.id.code_yellow_btn);
-        code_red = v.findViewById(R.id.code_red_btn);
+        TextView preventer = v.findViewById(R.id.preventer);
+        TextView m_puff = v.findViewById(R.id.m_puff);
+        TextView n_puff = v.findViewById(R.id.n_puff);
+        TextView reliever_name = v.findViewById(R.id.reliever_name);
+        TextView reliever_puff = v.findViewById(R.id.reliever_puff);
 
-        code_green.setOnClickListener(this);
-        code_yellow.setOnClickListener(this);
-        code_red .setOnClickListener(this);
+
+        preventer.setText("Example Medication");
+        m_puff.setText("3");
+        n_puff.setText("4");
+        reliever_name.setText("Example Medication");
+        reliever_puff.setText("6");
+
 
         return v;
-    }
-
-
-    @Override
-    public void onClick(View v) {
-        if( v == code_green) {
-            FragmentManager manager = getFragmentManager();
-            manager.beginTransaction().replace(R.id.fragment_code, new Code_green_Activity()).commit();
-        }
-
-        if ( v == code_yellow) {
-            FragmentManager manager = getFragmentManager();
-            manager.beginTransaction().replace(R.id.fragment_code, new Code_yellow_Activity()).commit();
-        }
-
-        if ( v == code_red) {
-            FragmentManager manager = getFragmentManager();
-            manager.beginTransaction().replace(R.id.fragment_code, new Code_red_Activity()).commit();
-        }
     }
 }
